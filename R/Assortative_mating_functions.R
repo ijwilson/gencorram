5305001e7baa5b9ea48e9a4965fa49d9c946f94d
wijjy-bio <ian.wilson.phd@gmail.com>
2021-03-04 19:41:51 +0000

    Add all files to github

---
+++ R/Assortative_mating_functions.R
@@ -0,0 +1,281 @@
+#' Sample the starting genotypes
+#'
+#' @param num_inds  Number of individuals
+#' @param allele_freq Allele frequency for each locus
+#'
+#' @return A matrix of allele counts, individuals in columns, loci in rows.
+#' @export
+#' @examples
+#' a <- sample_initial_genotypes(100, rep(0.5, 20))
+#' allele_freq <- runif(20, 0.01, 0.99)
+#' b <- sample_initial_genotypes(100, allele_freq))
+#' d <- rowMeans(b)/2
+#' plot(allele_freq, d)
+#'
+sample_initial_genotypes <- function(Num_inds, allele_freq) {
+  replicate(Num_inds,
+            rbinom(length(allele_freq), 2, allele_freq)
+            )
+}
+#'
+#' Get the genotype effects for each locus for two traits.
+#'
+#' The genotype effects consist of the effect for each locus, which is drawn from a Normal(0,1)
+#' distribution for each locus and the  genetic standard deviation of that trait which is the
+#' standard deviation for each trait normalised by the allele frequency.
+#'
+#' @param loci A vector with the loci affecting the trait
+#' @param allele_freq The allele frequency of sampled loci.
+#' A vector at least as long as max loci specified.
+#' @return A list with elements of the effect and the genetic standard deviation on the trait of class effects
+#' @export
+generate_effects <- function(loci, allele_freq, env_var) {
+  effects <- rnorm(length(loci))
+  genetic_sd <- sqrt(sum((effects^2) * 2 * allele_freq[loci] * (1 - allele_freq[loci])))
+  ee <- list(
+    loci=loci, effects = effects, gen_sd = genetic_sd, environ_var=env_var
+    )
+  class(ee) <- "effects"
+  return(ee)
+}
+#'
+#' Get correlated effects for a pair of traits.
+#'
+#' The genotype effects consist of the effect for each locus, which is drawn from a Normal(0,1)
+#' distribution for each locus and the genetic standard deviation of that trait which is the
+#' standard deviation for each trait normalised by the allele frequency.
+#'
+#' @param loci A vector with the loci affecting the traits
+#' @param allele_freq The allele frequency of sampled loci.
+#' A vector at least as long as max loci specified.
+#' @param corr_coeff The correlation coefficient between traits
+#' @return A a list with effects_A and effects_B
+#' @export
+generate_bio_pleiotrophy_effects <- function(loci, allele_freq, corr_coeff, env_var) {
+  if (length(env_var) == 1)
+    env_var <- c(env_var, env_var)
+  
+  effects <- mvtnorm::rmvnorm(length(loci), c(0, 0), 
+                              matrix(c(1, corr_coeff, corr_coeff, 1), ncol=2))
+  effects1 <- list(loci=loci, 
+                   effects = effects[,1], 
+                   gen_sd = sqrt(sum((effects[,1]^2) * 2 * allele_freq[loci] * (1 - allele_freq[loci]))),
+                   environ_var=env_var[1]
+  )
+  effects2 <- list(loci=loci, 
+                   effects = effects[, 2], 
+                   gen_sd = sqrt(sum((effects[, 2]^2) * 2 * allele_freq[loci] * (1 - allele_freq[loci]))),
+                   environ_var=env_var[2]
+  )
+  class(effects1) <- "effects"
+  class(effects2) <- "effects"
+  return(list(effects_A = effects1, effects_B = effects2))
+}
+
+#'
+#' Get the phenotype and the additive genetic part of the trait
+#'
+#' Gets the phenotype for each individual for the a genetic
+#' effect which comprises the additive genetic effect, and the
+#' environmental effect.e
+#'
+#' @param effects The effect size object
+#' @param genotype The genotypes with individuals in columns and sites in rows.
+#' @export
+#' @return A data frame with the phenotype and additive genetic variance.
+#'
+calculate_phenotype <-  function(eff, genotype) {
+  additive_genetic <- colSums(genotype[eff$loci, ] * eff$effects)
+
+  additive_genetic <- additive_genetic / eff$gen_sd
+  pheno <- additive_genetic +
+    rnorm(length(additive_genetic), mean = 0.0, sd = sqrt(eff$environ_var))
+  pheno <- scale(pheno, scale = FALSE) - mean(pheno)   # centre
+  return(data.frame(pheno = pheno, additive_genetic = additive_genetic))
+}
+
+#' Generate a gamete based on the parental genotype
+#'
+#' @param parental_genotype The genotype (as allele count) of the parent
+#' @export
+#' @return A vector with the haplotype (as allele count)
+
+make_a_gamete <- function(parental_genotype) {
+  u <- parental_genotype==1
+  hap <- parental_genotype/2        ## works for 0 and 2
+  hap[u] <- rbinom(sum(u), 1, 0.5)  ##
+  return(hap)
+}
+#'
+#' Get the next generation of genotypes
+#'
+#' @param mother_genotypes loci affecting trait A
+#' @param father_genotypes loci affecting trait B
+#' @export
+#' @examples
+#'
+#'
+next_generation <- function(mother_genotypes, father_genotypes) {
+  dad_gamete <- apply(father_genotypes, 2, make_a_gamete)
+  mum_gamete <- apply(mother_genotypes, 2, make_a_gamete)
+  return(dad_gamete + mum_gamete)
+}
+#'
+#' Get the next generation of Genotypes
+#'
+#' @param g a list containing male_genotype and female_genotype
+#' @param effects_A the effects for trait A
+#' @param effects_B the effects for trait B
+#' @param corr_coeff  Correlation coefficient between traits for assortative mating
+#' @export
+#' @examples
+#'
+#'
+generation <- function(g, effects_A, effects_B, corr_coeff) {
+  nmales <- ncol(g$male_genotype)
+  nfemales <- ncol(g$female_genotype)
+  female_phenotype <- calculate_phenotype(effects_A, g$female_genotype)
+  male_phenotype <-   calculate_phenotype(effects_B, g$male_genotype)
+
+  # Now select the dads.  This is assuming female mate choice (I believe)
+  parents_selected <- assort_mating(nmales+nfemales,
+                                    female_phenotype$pheno,
+                                      male_phenotype$pheno,
+                                      corr_coeff)
+
+  ## Generate - no ordering here so first half males and second half females.
+
+  child_genotype <- next_generation(
+    g$female_genotype[, parents_selected$mothers_index],
+    g$male_genotype[, parents_selected$fathers_index])
+
+  list(female_genotype=child_genotype[,1:nfemales],
+       male_genotype = child_genotype[, (nfemales+1):(nmales+nfemales)])
+}
+#'
+#' Get the next generation of Genotypes
+#'
+#' @param nchildren The number of children to generate
+#' @param g a list containing male_genotype and female_genotype
+#' @param effects_A the effects for trait A
+#' @param effects_B the effects for trait B
+#' @param corr_coeff  Correlation coefficient between traits for assortative mating
+#' @export
+#' @examples
+#'
+#'
+sexual_reproduction <- function(nchildren, g, effects_A, effects_B, corr_coeff) {
+  nmales <- ncol(g$male_genotype)
+  nfemales <- ncol(g$female_genotype)
+  nloci <- nrow(g$male_genotype)
+  female_phenotype <- calculate_phenotype(effects_A, g$female_genotype)
+  male_phenotype <-   calculate_phenotype(effects_B, g$male_genotype)
+  
+  # Now select the dads.  This is assuming female mate choice (I believe)
+  parents_selected <- assort_mating(nchildren,
+                                    female_phenotype$pheno,
+                                    male_phenotype$pheno,
+                                    corr_coeff)
+  # This is not finished yet but can be speeded up considerably
+  # by using Rcpp
+  gg <- matrix(integer(0), ncol=nchildren, nrow=nloci)
+  for (i in 1:nchildren) {
+    code <- g$female_genotype[, parents_selected$mothers_index] + 3*g$male_genotype[, parents_selected$fathers_index]
+    
+  }
+  
+  ## Generate - no ordering here so first half males and second half females.
+  
+  child_genotype <- next_generation(
+    g$female_genotype[, parents_selected$mothers_index],
+    g$male_genotype[, parents_selected$fathers_index])
+  
+  list(female_genotype=child_genotype[,1:nfemales],
+       male_genotype = child_genotype[, (nfemales+1):(nmales+nfemales)])
+}
+
+
+#' Initialise the genotypes and effects and get a set of male and female genotypes
+#'
+#' @param population_size The size of the population to simulate
+#' @param loci_A Loci affecting trait A only
+#' @param loci_B Loci affecting trait B only
+#' @param loci_AB Loci affecting traits A and B
+#' @param corr_coefficient Genetic correlation between traits A and B based on shared loci 
+#' @export
+#'
+init <- function(population_size,
+                       loci_A,       # loci affecting trait A only
+                       loci_B,       # loci affecting trait B only
+                    corr_coeff,
+                       environ_var = c(1,1),  # environmental variance
+                       n_loci,
+                       allele_freq = 0.5) {
+  # Need to check the loci selected.
+  if (missing(n_loci)) {
+    if (length(allele_freq) > 1) {
+      n_loci <- length(allele_freq)
+      if (max(loci_A) > n_loci | max(loci_B) > n_loci)
+        stop("if you set allele_freq then it must be long enough to include loci for traits A and B")
+    } else {
+      n_loci <- max(c(loci_A, loci_B))
+    }
+  } else {
+    if (max(loci_A) > n_loci | max(loci_B) > n_loci)
+      stop("loci for traits A and B must be in the range 1:n_loci")
+  }
+
+  if (length(allele_freq) == 1) {
+    allele_freq <- rep(allele_freq, n_loci)   ## Start all alleles at frequency 0.5
+  } else {
+    if (n_loci != length(allele_freq))
+      stop("allele freq must have length 1 or the same length as n_loci")
+  }
+
+
+  ## Make the parental generation
+  female_genotype <-  sample_initial_genotypes(population_size, allele_freq)
+  male_genotype   <-  sample_initial_genotypes(population_size, allele_freq)
+  ## generate a set of genetic effects
+  effects_A <- generate_effects(loci_A, allele_freq, environ_var) ## generate effect sizes for two different traits
+  effects_B <- generate_effects(loci_B, allele_freq, environ_var) ## generate effect sizes for two different traits
+  ## Get the female and male phenotypes in the initial generation
+
+  list(
+    male_genotype=male_genotype,
+    female_genotype=female_genotype,
+    effects_A=effects_A, 
+    effects_B=effects_B
+    )
+}
+
+#' Generate the pairs selected by mothers
+#'
+#' @param pop_size The number of pairs to generate.
+#' @param female_phenotype A numeric vector giving the phenotypes of all females
+#' @param male_phenotype A numeric phenotype giving the phenotypes of all males
+#' @param corr_coeff The correlation coefficient between phenotypes for males and females
+#' @export
+#' @return the indices of the returned mothers and fathers
+assort_mating <-   function(popsize,
+                            female_phenotype,
+                            male_phenotype,
+                            corr_coeff) {
+
+  n_females <- length(female_phenotype)
+  n_males <- length(male_phenotype)
+  nn <- mvtnorm::rmvnorm(popsize, c(0,0), matrix(c(1, corr_coeff, corr_coeff, 1), ncol=2))
+  pp <- pnorm(nn)   
+  
+  om <- order(male_phenotype)
+  of <- order(female_phenotype)
+  
+  data.frame(mothers_index=of[1+floor(pp[,1]*n_females)],
+                    fathers_index=om[1+floor(pp[,2]*n_males)])
+}
+
+
+
+#' Code rewritten from that on Joe Pickrell blog
+#'
+#' https://github.com/joepickrell/rg-post/blob/master/Assortative_mating_sims.R
---
+++ R/am_sims.R
@@ -0,0 +1,408 @@
+#'
+#' Generate father mother pairs
+#'
+#' This is really just a test that the simulations and the
+#' correlation coefficients work.
+#'
+#' @param population_size The Size of the populations to simulate.  Equal numbers of males and
+#' females of this size.
+#' @param loci_A loci affecting trait A
+#' @param loci_B loci affecting trait B
+#' @param environ_var environmental variance
+#' @param corr_coeff Correlation coefficient between phenotype in males and females
+#' @param n_loci The number of loci to simulate.  Not always needed.
+#' @param allele_freq The allele frequency of sampled loci.
+#' Either a single value or a vector at least as long as max loci specified.
+#' @export
+#' @examples
+#' g1 <- generate_QT_assortative_mating(population_size = 100000, loci_A = 1:100, loci_B = 101:200,
+#'                                      environ_var = 0.3, corr_coeff = 0.3)
+#' cor(g1$mother_phenotype$pheno, g1$father_phenotype$pheno)#
+#' plot(g1$mother_phenotype$pheno, g1$father_phenotype$pheno)
+#' # Simulate for loci with rarer variants
+#' g2 <- generate_QT_assortative_mating(population_size = 100000, loci_A = 1:100, loci_B = 101:200,
+#'                          environ_var = 0.3, corr_coeff = 0.3, allele_freq = runif(200, 0.01, 0.05))
+#' cor(g2$mother_phenotype$pheno, g2$father_phenotype$pheno)
+#' plot(g2$mother_phenotype$pheno, g2$father_phenotype$pheno)
+#'
+generate_QT_assortative_mating <- function(
+  population_size,
+  loci_A, loci_B,
+  environ_var,  # environmental variance
+  corr_coeff = 0,
+  n_loci,
+  allele_freq = 0.5) {
+
+  g <- init(population_size, loci_A, loci_B, environ_var, n_loci, allele_freq)
+
+  ## Get the female and male phenotypes in the initial generation
+  female_phenotype <- calculate_phenotype(g$effects_A, g$female_genotype)
+  male_phenotype <-   calculate_phenotype(g$effects_B, g$male_genotype)
+
+  # Now select the mums and dads.  This is assuming female mate choice (I believe)
+  parents <- assort_mating(2*population_size, female_phenotype$pheno,
+                           male_phenotype$pheno, corr_coeff)
+
+  mother_genotype  <- g$female_genotype[, parents$mothers_index]
+  father_genotype <- g$male_genotype[, parents$fathers_index]
+
+  print(head(female_phenotype))
+
+  return(
+    list(
+      mother_geno = mother_genotype,
+      father_geno = father_genotype,
+      effects_A = g$effects_A,
+      effects_B = g$effects_B,
+      mother_phenotype = female_phenotype[parents$mothers_index,],
+      father_phenotype = male_phenotype[parents$fathers_index,],
+      allele_freq=allele_freq)
+  )
+}
+
+#' Provide a quick report on the simulation of parental pairs and a child.
+#'
+#' This report is only for a single generation
+#'
+#' @param population_size The Size of the populations to simulate.  Equal numbers of males and
+#' females of this size.
+#' @param loci_A loci affecting trait A
+#' @param loci_B loci affecting trait B
+#' @param environ_var environmental variance
+#' @param corr_coeff Correlation coefficient between phenotype in males and females
+#' @param n_loci The number of loci to simulate.  Not always needed.
+#' @param allele_freq The allele frequency of sampled loci.
+#' Either a single value or a vector at least as long as max loci specified.
+#' @export
+
+QT_assortative_mating_report <- function(population_size,
+                                  loci_A,       # loci affecting trait A
+                                  loci_B,       # loci affecting trait B
+                                  environ_var,  # environmental variance
+                                  corr_coeff = 0,
+                                  n_loci,
+                                  allele_freq = 0.5,
+                                  plots = FALSE,
+                                  write_plots = FALSE) {
+
+  g <- generate_QT_assortative_mating(population_size,
+                                      loci_A, loci_B,
+                                      environ_var, corr_coeff, n_loci, allele_freq)
+
+  print(names(g))
+
+  L <- length(allele_freq)
+  cor_pheno <- cor(g$mother_phenotype$pheno, g$father_phenotype$pheno)
+  cat("correlation coefficient between parental phenotypes", cor_pheno, "\n")
+  cor_add_gen <- cor(g$mother_phenotype$additive_genetic, g$father_phenotype$additive_genetic)
+  cat("correlation coefficient between parental additive phenotype", cor_add_gen, "\n")
+
+  layout(t(1:2))
+
+  plot(
+    g$mother_phenotype$pheno, g$father_phenotype$pheno,
+    xlab = "Female Phenotype",
+    ylab = "Male Phenotype",
+    col = adjustcolor("black", 0.2),
+    main = paste("Corr. parental phenotype=",  corr_coeff)
+  )
+  abline(lm(g$father_phenotype$pheno ~ g$mother_phenotype$pheno, col = "red"))
+  text(x = -2,
+       y = 2,
+       format(cor(g$father_phenotype$pheno, g$mother_phenotype$pheno), dig = 2),
+       col = "red")
+
+  plot(
+    g$mother_phenotype$additive_genetic, g$father_phenotype$additive_genetic,
+    xlab = "Female Additive Genetic Score A",
+    ylab = "Male Additive Genetic Score B",
+    col = adjustcolor("black", 0.2),
+    main = paste("Corr. parental additive genetic score=",  corr_coeff)
+  )
+  abline(lm(g$father_phenotype$additive_genetic ~ g$mother_phenotype$additive_genetic, col = "red"))
+  text(x = -2,
+       y = 2,
+       format(cor(g$father_phenotype$additive_genetic, g$mother_phenotype$additive_genetic), dig = 2),
+       col = "red")
+
+
+
+  plot(
+    colSums(g$mother_geno) / L,
+    colSums(g$father_geno) / L,
+    xlab = "Female Mean Genotype",
+    ylab = "Male Mean Genotype",
+    col = adjustcolor("black", 0.2),
+    main = paste(
+      "Cor. parental pheno.=",
+      corr_coeff,
+      "Heritability",
+      format(1 / (1 + environ_var), digits = 2)
+    )
+  )
+  dad.mean <- colSums(g$father_geno) / L
+  mum.mean <- colSums(g$mother_geno) / L
+  abline(lm(dad.mean ~ mum.mean), col = "red")
+  text(x = 0.5,
+       y = 1.5,
+       format(cor(mum.mean, dad.mean), dig = 2),
+       col = "red")
+
+
+
+  ### Make a child
+
+  ###CHECK THIS, WILL NEED TO DO TWICE ONCE FOR DAUGHTER ONCE FOR SONS?
+  dad_gamete <- apply(g$father_geno, 2, make_a_gamete)
+  mum_gamete <- apply(g$mother_geno, 2, make_a_gamete)
+  child_genotype <- dad_gamete + mum_gamete
+  cat(mean(child_genotype == 0),
+      mean(child_genotype == 1),
+      mean(child_genotype == 2),
+      "\n")
+
+  childs_phenotype_A <-
+    calculate_phenotype(eff = g$effects_A, child_genotype)
+  childs_phenotype_B <-
+    calculate_phenotype(eff = g$effect_B, child_genotype)
+
+  tmp1 <- (apply(child_genotype[loci_A, ], 1, function(test.geno) {
+    summary(lm(childs_phenotype_A[, 1] ~ test.geno))$coeff[2, ]
+  }))
+  tmp2 <- (apply(child_genotype[loci_A,], 1, function(test.geno) {
+    summary(lm(childs_phenotype_A[, 1] ~ test.geno))$coeff[2, ]
+  }))
+  tmp3 <- (apply(child_genotype[loci_B, ], 1, function(test.geno) {
+    summary(lm(childs_phenotype_B[, 1] ~ test.geno))$coeff[2, ]
+  }))
+  tmp4 <- (apply(child_genotype[loci_B,], 1, function(test.geno) {
+    summary(lm(childs_phenotype_B[, 1] ~ test.geno))$coeff[2, ]
+  }))
+  #	col <- c(rep("blue", length(loci_A)), rep("green", length(loci_B)))
+  plot(
+    tmp1[1, ],
+    tmp2[1, ],
+    xlab = "effect size trait 1",
+    ylab = "effect size trait 2",
+    cex.lab = 1.5
+    ,
+    pch = 19,
+    col = "green",
+    xlim = range(c(tmp1[1, ], tmp2[1, ])),
+    ylim = range(c(tmp2[1, ], tmp4[1, ]))
+  )
+  points(
+    tmp3[1, ],
+    tmp4[1, ],
+    xlab = "effect size trait 1",
+    ylab = "effect size trait 2",
+    cex.lab = 1.5
+    ,
+    pch = 19,
+    col = "blue"
+  )
+
+  print(summary(lm(tmp1[1, ] ~ tmp2[1, ])))
+  print(summary(lm(tmp3[1, ] ~ tmp4[1, ])))
+
+
+}
+
+#' Provide a quick report on the simulation of parental pairs and a child.
+#'
+#' This report is only for a single generation
+#' @param am_corr_coeff The associative mating correlation coefficent
+#' @param ntrait_loci  The number of loci for each trait
+#' @param n_loci The number of loci to simulate.  
+#' @param environ_var environmental variance.  A vector of length 1 or 2.
+#' @param pop_size The Size of the populations to simulate.  Equal numbers of males and
+#' females of this size.
+#' @param generations The number of generations to run the simulation.
+#' @param ntraining The size of the training data set
+#' @param af_r_func A function of one variable (n) that returns a vector of allele frequencies of length n
+#' @export
+simulate_gen_assoc <- function(
+  am_corr_coeff, 
+  ntrait_loci,
+  nloci,
+  envir_var,
+  pop_size,
+  generations,
+  ntraining,
+  af_r_func = runif) {
+  
+  allele_freq <- af_r_func(nloci)                         
+  if (length(envir_var) ==1) envir_var <- rep(envir_var, 2)
+  ## simulate training dataset for lasso 
+  training <- sample_initial_genotypes(ntraining, allele_freq)
+  
+  trait_loci <- sample(nloci, 2*ntrait_loci)
+  trait_loci_A <- trait_loci[1:ntrait_loci]
+  trait_loci_B <- trait_loci[(ntrait_loci+1):(2*ntrait_loci)]
+  
+  
+  # get the  effects for the traits
+  effects_A <- generate_effects(trait_loci_A, allele_freq, env_var = envir_var[1])
+  effects_B <- generate_effects(trait_loci_B, allele_freq, env_var = envir_var[2])
+  # and phenotypes for training
+  training_pheno_A <- calculate_phenotype(effects_A, training)$pheno
+  training_pheno_B <- calculate_phenotype(effects_B, training)$pheno
+  # this is an example estimated by doing a bigger analysis that takes a long time
+  fitted_lambda <- 0.02310998
+  lasso_model_A <-  glmnet::glmnet(t(training), training_pheno_A, alpha = 1, lambda = fitted_lambda)
+  lasso_model_B <-  glmnet::glmnet(t(training), training_pheno_B, alpha = 1, lambda = fitted_lambda)
+  
+  rm(training, training_pheno_A, training_pheno_B)
+  
+  ## A function to collect standard deviations and correlations
+  # This one is more complicated as it collects all standard deviation and all correlations named
+  summarise_phenotypes3 <- function(gg, effects_A, effects_B, lasso_model_A, lasso_model_B) {
+    ga <- cbind(gg$male_genotype, gg$female_genotype)
+    A <- setNames(calculate_phenotype(effects_A, ga), c("phenoA", "addgenA"))
+    lasso_A <- predict(lasso_model_A, t(ga))
+    B  <- setNames(calculate_phenotype(effects_B, ga), c("phenoB", "addgenB"))
+    lasso_B <- predict(lasso_model_B, t(ga))
+    cr <- cor(cbind(A, lassoA=lasso_A[, 1], B, lassoB=lasso_B[,1]))
+    v <- var(cbind(A, lassoA=lasso_A[, 1], B, lassoB=lasso_B[,1]))
+    
+    nn <- outer(colnames(v), colnames(v), paste, sep="_")
+    crr <- cr[upper.tri(cr, diag=FALSE)]
+    names(crr) <- nn[upper.tri(cr, diag=FALSE)]
+    c(sqrt(diag(v)), crr[c(4, 8, 13)])
+  
+  }
+  
+  ## ----simulate_generations---------------------
+  
+  go <- list(male_genotype = sample_initial_genotypes(pop_size, allele_freq),
+             female_genotype = sample_initial_genotypes(pop_size, allele_freq))
+  
+  p <- list()
+  p[[1]] <- summarise_phenotypes3(go, effects_A, effects_B, lasso_model_A, lasso_model_B)
+  for (i in 1:generations) {
+  #  if (i%%5 == 0) cat("generation", i, "\n")
+    gn <- generation(go, effects_A, effects_B, am_corr_coeff)
+    go <- gn
+    p[[i+1]] <- summarise_phenotypes3(gn, effects_A, effects_B, lasso_model_A, lasso_model_B)
+  }
+  return(
+    setNames(
+      data.frame(matrix(unlist(p), nrow = 1+generations, byrow=TRUE)),
+      names(p[[1]])
+    )
+  )
+}
+
+
+
+#' Provide a quick report on the simulation of parental pairs and a child.
+#'
+#' This report is only for a single generation
+#' @param am_corr_coeff The associative mating correlation coefficent
+#' @param ntrait_loci  The number of loci for each trait
+#' @param n_loci The number of loci to simulate.  
+#' @param environ_var environmental variance.  A vector of length 1 or 2.
+#' @param pop_size The Size of the populations to simulate.  Equal numbers of males and
+#' females of this size.
+#' @param generations1 The number of generations to run the simulation before the break.
+#' @param generations2 The number of generations after the two new polygenic scores are calculated
+#' @param ntraining The size of the training data set
+#' @param af_r_func A function of one variable (n) that returns a vector of allele frequencies of length n
+#' @export
+simulate_gen_assoc_recalc_PGS <- function(
+  am_corr_coeff, 
+  ntrait_loci,
+  nloci,
+  envir_var,
+  pop_size,
+  generations1,
+  generations2,
+  ntraining,
+  af_r_func = runif) {
+  ## A function to collect standard deviations and correlations
+  # This one is more complicated as it collects all standard deviation and all correlations named
+  summarise_phenotypes5 <- function(gg, effects_A, effects_B, lasso_model_A, lasso_model_B, lasso_model_A2=NULL, lasso_model_B2=NULL) {
+    ga <- cbind(gg$male_genotype, gg$female_genotype)
+    A <- setNames(calculate_phenotype(effects_A, ga), c("phenoA", "addgenA"))
+    lasso_A <- predict(lasso_model_A, t(ga))[, 1]
+    B  <- setNames(calculate_phenotype(effects_B, ga), c("phenoB", "addgenB"))
+    lasso_B <- predict(lasso_model_B, t(ga))[, 1]
+    
+    if (!is.null(lasso_model_A2))
+      lasso_A2 <- predict(lasso_model_A2, t(ga))[, 1]
+    else
+      lasso_A2 <- rep(NA, length(lasso_A))
+    
+    if (!is.null(lasso_model_B2))
+      lasso_B2 <- predict(lasso_model_B2, t(ga))[, 1]
+    else
+      lasso_B2 <- rep(NA, length(lasso_A))
+    
+    xxx <- cbind(A, lassoA=lasso_A, B, lassoB=lasso_B, lassoA2 = lasso_A2, lassoB2=lasso_B2)
+    cr <- cor(xxx)
+    v <-  var(xxx)
+    
+    nn <- outer(colnames(v), colnames(v), paste, sep="_")
+    crr <- cr[upper.tri(cr, diag=FALSE)]
+    names(crr) <- nn[upper.tri(cr, diag=FALSE)]
+    c(sqrt(diag(v)), crr)
+  }
+  
+  allele_freq <- af_r_func(nloci)                         
+  if (length(envir_var) ==1) envir_var <- rep(envir_var, 2)
+  ## simulate training dataset for lasso 
+  training <- sample_initial_genotypes(ntraining, allele_freq)
+  
+  trait_loci <- sample(nloci, 2*ntrait_loci)
+  trait_loci_A <- trait_loci[1:ntrait_loci]
+  trait_loci_B <- trait_loci[(ntrait_loci+1):(2*ntrait_loci)]
+  
+  # get the  effects for the traits
+  effects_A <- generate_effects(trait_loci_A, allele_freq, env_var = envir_var[1])
+  effects_B <- generate_effects(trait_loci_B, allele_freq, env_var = envir_var[2])
+  # and phenotypes for training
+  training_pheno_A <- calculate_phenotype(effects_A, training)$pheno
+  training_pheno_B <- calculate_phenotype(effects_B, training)$pheno
+  # this is an example estimated by doing a bigger analysis that takes a long time
+  fitted_lambda <- 0.02310998
+  lasso_model_A <-  glmnet::glmnet(t(training), training_pheno_A, alpha = 1, lambda = fitted_lambda)
+  lasso_model_B <-  glmnet::glmnet(t(training), training_pheno_B, alpha = 1, lambda = fitted_lambda)
+  
+  rm(training, training_pheno_A, training_pheno_B)
+  
+
+  ## ----simulate_generations---------------------
+  
+  go <- list(male_genotype = sample_initial_genotypes(pop_size, allele_freq),
+             female_genotype = sample_initial_genotypes(pop_size, allele_freq))
+  
+  p <- list()
+  p[[1]] <- summarise_phenotypes5(go, effects_A, effects_B, lasso_model_A, lasso_model_B, NULL, NULL)
+  for (i in 1:generations1) {
+    #  if (i%%5 == 0) cat("generation", i, "\n")
+    gn <- generation(go, effects_A, effects_B, am_corr_coeff)
+    go <- gn
+    p[[i+1]] <- summarise_phenotypes5(gn, effects_A, effects_B, lasso_model_A, lasso_model_B)
+  }
+  
+  training2 <- cbind(gn$male_genotype, gn$female_genotype)
+  training_pheno_A2 <- calculate_phenotype(effects_A, training2)$pheno
+  training_pheno_B2 <- calculate_phenotype(effects_B, training2)$pheno
+  
+  lasso_model_A2 <-  glmnet::glmnet(t(training2), training_pheno_A2, alpha = 1, lambda = fitted_lambda)
+  lasso_model_B2 <-  glmnet::glmnet(t(training2), training_pheno_B2, alpha = 1, lambda = fitted_lambda)
+  
+  rm(training2, training_pheno_B2, training_pheno_A2)
+  
+  for (i in (generations1+1):(generations1+generations2)) {
+    gn <- generation(go, effects_A, effects_B, am_corr_coeff)
+    go <- gn
+    p[[i+1]] <- summarise_phenotypes5(gn, effects_A, effects_B, lasso_model_A, lasso_model_B, lasso_model_A2, lasso_model_B2)
+  }
+  return(
+    setNames(
+      data.frame(matrix(unlist(p), nrow = 1+generations1+generations2, byrow=TRUE)),
+      names(p[[1]])
+    )[,c(1:8, 12, 16, 21, 26, 35,36)]
+  )
+}
